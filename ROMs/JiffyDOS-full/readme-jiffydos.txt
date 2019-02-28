dc9dc90e5401e497e20e787f91ef721b7d1aa591
Sven <svpetry@gmail.com>
2019-02-28 20:06:54 +0100

    Added ROMs, ROMBuilder and documentation.

---
+++ ROMs/JiffyDOS-full/readme-jiffydos.txt
@@ -0,0 +1,7 @@
+JiffyDOS_C1541.bin is a 16K file, for the 1541
+
+JiffyDOS_1541.bin is the upper half, and replaces 901229-0x on older 1541 drives.
+
+
+For the 1581 ROM use a 27(C)256.  The Original 1581 ROM is 318045-01 aka 312558-01.  318045-02.bin fixes a few bugs.   
+
---
+++ ROMs/RomBuilder/RomBuilder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rom = new byte[0xFFFF];
+
+            AddRom(rom, "kernal.901227-03.bin", 0xA000); // kernal 1
+            AddRom(rom, "JiffyDOS_C64.bin", 0x2000); // kernal 2
+            AddRom(rom, "EXOS.ROM", 0x8000); // kernal 3
+            AddRom(rom, "CockroachROM.bin", 0x0000); // kernal 4
+
+            AddRom(rom, "basic.901226-01.bin", 0xC000);
+            AddRom(rom, "characters.901225-01.bin", 0x6000);
+
+            File.WriteAllBytes("ROM.bin", rom);
+        }
+
+        private static void AddRom(byte[] rom, string fileName, int address)
+        {
+            var fileBytes = File.ReadAllBytes(fileName);
+            for (var i = 0; i < fileBytes.Length; i++)
+                rom[address + i] = fileBytes[i];
+        }
+    }
+}
