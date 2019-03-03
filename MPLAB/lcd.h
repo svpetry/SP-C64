312cc3d7dc486737e2d57dadbda8edc6dcb831c7
Sven Petry <svpetry@gmail.com>
2019-03-03 21:48:59 +0100

    Corrected whitespace.

--- MPLAB/lcd.h
+++ MPLAB/lcd.h
@@ -3,22 +3,22 @@
 
 void Lcd_Port(char a)
 {
-	if(a & 1)
+	if (a & 1)
 		D4 = 1;
 	else
 		D4 = 0;
 
-	if(a & 2)
+	if (a & 2)
 		D5 = 1;
 	else
 		D5 = 0;
 
-	if(a & 4)
+	if (a & 4)
 		D6 = 1;
 	else
 		D6 = 0;
 
-	if(a & 8)
+	if (a & 8)
 		D7 = 1;
 	else
 		D7 = 0;
@@ -27,9 +27,9 @@ void Lcd_Cmd(char a)
 {
 	RS = 0;             // => RS = 0
 	Lcd_Port(a);
-	EN  = 1;             // => E = 1
-        __delay_ms(4);
-        EN  = 0;             // => E = 0
+	EN  = 1;            // => E = 1
+    __delay_ms(4);
+    EN  = 0;            // => E = 0
 }
 
 void Lcd_Clear()
@@ -40,16 +40,16 @@ void Lcd_Clear()
 
 void Lcd_Set_Cursor(char a, char b)
 {
-	char temp,z,y;
-	if(a == 1)
+	char temp, z, y;
+	if (a == 1)
 	{
-	  temp = 0x80 + b - 1;
+        temp = 0x80 + b - 1;
 		z = temp>>4;
 		y = temp & 0x0F;
 		Lcd_Cmd(z);
 		Lcd_Cmd(y);
 	}
-	else if(a == 2)
+	else if (a == 2)
 	{
 		temp = 0xC0 + b - 1;
 		z = temp>>4;
@@ -61,21 +61,21 @@ void Lcd_Set_Cursor(char a, char b)
 
 void Lcd_Init()
 {
-  Lcd_Port(0x00);
-   __delay_ms(20);
-  Lcd_Cmd(0x03);
-	__delay_ms(5);
-  Lcd_Cmd(0x03);
-	__delay_ms(11);
-  Lcd_Cmd(0x03);
-  /////////////////////////////////////////////////////
-  Lcd_Cmd(0x02);
-  Lcd_Cmd(0x02);
-  Lcd_Cmd(0x08);
-  Lcd_Cmd(0x00);
-  Lcd_Cmd(0x0C);
-  Lcd_Cmd(0x00);
-  Lcd_Cmd(0x06);
+    Lcd_Port(0x00);
+    __delay_ms(20);
+    Lcd_Cmd(0x03);
+    __delay_ms(5);
+    Lcd_Cmd(0x03);
+    __delay_ms(11);
+    Lcd_Cmd(0x03);
+    /////////////////////////////////////////////////////
+    Lcd_Cmd(0x02);
+    Lcd_Cmd(0x02);
+    Lcd_Cmd(0x08);
+    Lcd_Cmd(0x00);
+    Lcd_Cmd(0x0C);
+    Lcd_Cmd(0x00);
+    Lcd_Cmd(0x06);
 }
 
 void Lcd_Write_Char(char a)
@@ -97,7 +97,7 @@ void Lcd_Write_Char(char a)
 void Lcd_Write_String(char *a)
 {
 	int i;
-	for(i=0;a[i]!='\0';i++)
+	for (i=0; a[i]!='\0'; i++)
 	   Lcd_Write_Char(a[i]);
 }
 
