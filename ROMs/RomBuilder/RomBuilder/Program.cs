b056143d17a764da6bab034c7f2db404d543b354
Sven Petry <svpetry@gmail.com>
2019-04-25 21:09:03 +0200

    Added source documentation for RomBuilder.

--- ROMs/RomBuilder/RomBuilder/Program.cs
+++ ROMs/RomBuilder/RomBuilder/Program.cs
@@ -1,21 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace RomBuilder
 {
     class Program
     {
+        /// <summary>
+        /// A15,A14,A13     address     name
+        /// 
+        ///     000         0x0000      KERNAL 4
+        ///     001         0x2000      KERNAL 3
+        ///     010         0x4000  
+        ///     011         0x6000      CHAROM
+        ///     100         0x8000      KERNAL 2
+        ///     101         0xA000      KERNAL 1
+        ///     110         0xC000      BASIC
+        ///     111         0xE000  
+        /// </summary>
+
         static void Main(string[] args)
         {
             var rom = new byte[0xFFFF];
 
             AddRom(rom, "kernal.901227-03.bin", 0xA000); // kernal 1
-            AddRom(rom, "JiffyDOS_C64.bin", 0x2000); // kernal 2
-            AddRom(rom, "EXOS.ROM", 0x8000); // kernal 3
+            AddRom(rom, "JiffyDOS_C64.bin", 0x8000); // kernal 2
+            AddRom(rom, "EXOS.ROM", 0x2000); // kernal 3
             AddRom(rom, "CockroachROM.bin", 0x0000); // kernal 4
 
             AddRom(rom, "basic.901226-01.bin", 0xC000);
